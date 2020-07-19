79ddf2b3bf817eac88bb740bfcc7d1c98a102e91
geowatson <georgy.poputnikov@x5.ru>
2020-07-19 23:39:40 +0300

    added few more methods

--- main.go
+++ main.go
@@ -1,32 +1,68 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"io"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"runtime"
+	"strconv"
+	"strings"
 	"time"
 )
 
 type CommandFunc func(f *os.File) string
+type Devices struct {
+	Devices []Device
+}
+type Device struct {
+	Id string `json:"id"`
+	Name string `json:"name"`
+	IsActive bool `json:"is_active"`
+}
 
+type FeaturedPlaylistsResponse struct {
+	Message string `json:"message"`
+	Playlists Playlists `json:"playlists"`
+}
+
+type Playlists struct {
+	Items []Playlist
+	Total int
+}
+
+type Playlist struct {
+	Id string `json:"id"`
+	Name string `json:"name"`
+	Description string `json:"description"`
+}
+
+const ServletPort = "7911"
+const ClientToken = ""  // can be retrieved from https://developer.spotify.com/dashboard/applications
 const BaseUrl = "https://api.spotify.com/v1"
 const SecretPattern = "secret-spotify-cli-*.txt"
-const ClientToken = ""
+
 
 var WaitForServer = true
+var CurrentToken string
 
 func getCommands() map[string]CommandFunc {
 	return map[string]CommandFunc{
 		"login": login,
 		"next":  nextTrack,
+		"device": selectDevice,
+		"random": playRandomSong,
 	}
 }
 
@@ -51,7 +87,7 @@ func login(file *os.File) string {
 	})
 
 	srv := &http.Server{
-		Addr:    ":8080",
+		Addr:    ":" + ServletPort,
 		Handler: r,
 	}
 
@@ -63,13 +99,13 @@ func login(file *os.File) string {
 
 	go func() {
 		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatal("kek!")
+			log.Fatal(err)
 		}
 	}()
 	time.Sleep(1 * time.Second)
 	openBrowser("https://accounts.spotify.com/en/authorize" +
 		"?client_id=" + ClientToken +
-		"&redirect_uri=http://localhost:8080/ok" +
+		"&redirect_uri=http://localhost:" + ServletPort + "/ok" +
 		"&response_type=token" +
 		"&scope=ugc-image-upload user-read-playback-state user-modify-playback-state user-read-currently-playing streaming app-remote-control")
 
@@ -89,23 +125,184 @@ func nextTrack(file *os.File) string {
 	}
 
 	path := "/me/player/next"
-	client := &http.Client{}
-	req, err := http.NewRequest("POST", BaseUrl + path, nil)
-	if err != nil {
-		log.Fatal("Cannot create request to connect to " + path)
+	headers := map[string]string{
+		"Authorization": "Bearer " + token,
 	}
-	req.Header.Add("Authorization", "Bearer " + token)
-	response, err := client.Do(req)
+
+	response, err := makeRequest("POST", BaseUrl + path, headers, nil)
 	if err != nil {
 		return "Cannot move to the next song :("
 	}
+
 	if response.StatusCode < 300 {
 		return "Playing next"
-	} else if response.StatusCode == http.StatusUnauthorized {
+	}
+	if response.StatusCode == http.StatusUnauthorized {
 		return "You need to re-login."
 	}
 	return "Cannot move to the next song :("
 }
+
+func selectDevice(file *os.File) string {
+	devices, err := getDevices(file)
+	if err != nil {
+		log.Fatal(err)
+		return "Something bad happened while getting Devices"
+	}
+	if len(devices) == 0 {
+		return "No available devices. Open Spotify app on any of your devices!"
+	}
+	if len(devices) == 1 {
+		return "Currently available only \"" + devices[0].Name + "\""
+	}
+	println("Available devices:")
+	s := ""
+	for i, v := range devices {
+		t := "[" + strconv.Itoa(i) + "] " + v.Name
+		if v.IsActive {
+			t += " (current)"
+		}
+		s += t + "\n"
+	}
+	print(s)
+	print("Select device by its id (enclosed in []): ")
+	reader := bufio.NewReader(os.Stdin)
+	text, err := reader.ReadString('\n')
+	text = strings.Replace(text, "\n", "", -1)
+	if err != nil {
+		return "Whoops! cannot read this line"
+	}
+	deviceId, err := strconv.Atoi(text)
+	if err != nil || deviceId < 0 || deviceId > len(devices) - 1 {
+		return "Malformed input"
+	}
+	if devices[deviceId].IsActive {
+		return "Already listening on this device"
+	}
+	res, err := setDevice(CurrentToken, devices[deviceId].Id)
+	if err != nil {
+		return "Cannot change to the selected device."
+	}
+
+	return res
+}
+
+func setDevice(token string, deviceId string) (s string, err error) {
+	path := "/me/player"
+	headers := map[string]string{
+		"Authorization": "Bearer " + token,
+	}
+	body := map[string]interface{}{
+		"device_ids": []interface{}{deviceId},
+		"play": true,
+	}
+
+	if res, err := makeRequest("PUT", BaseUrl + path, headers, body); err != nil || (res != nil && res.StatusCode > 399) {
+		return "Couldn't change the device due to an unexpected error", err
+	}
+
+	return "Successfully changed device", nil
+}
+
+func getDevices(file *os.File) (devices []Device, err error) {
+	token, err := getToken(file)
+	if err != nil {
+		return nil, err
+	}
+
+	path := "/me/player/devices"
+	headers := map[string]string{
+		"Authorization": "Bearer " + token,
+	}
+
+	response, err := makeRequest("GET", BaseUrl + path, headers, nil)
+	if err != nil {
+		return nil, err
+	}
+	if response.StatusCode == http.StatusUnauthorized {
+		return nil, errors.New("you need to re-login")
+	}
+	tempBody, _ := ioutil.ReadAll(response.Body)
+	var resBody Devices
+	if jsonErr := json.Unmarshal(tempBody, &resBody); jsonErr != nil {
+		return nil, jsonErr
+	}
+
+	return resBody.Devices, nil
+}
+
+func playRandomSong(file *os.File) string {
+	rand.Seed(time.Now().UnixNano())
+
+	if _, err := getToken(file); err != nil {
+		return "You need to log-in."
+	}
+
+	p, err := getFeaturedPlaylists()
+	if err != nil {
+		return "Cannot get featured playlists, reason: " + err.Error()
+	}
+	randPos := rand.Intn(len(p))
+
+	if err := play("playlist", p[randPos].Id); err != nil {
+		return "Cannot play random song :("
+	}
+
+	return "Playing for you now: [Playlist] " + p[randPos].Name + " - " + p[randPos].Description
+}
+
+func play(playType string, playId string) error {
+	path := "/me/player/play"
+	headers := map[string]string{
+		"Authorization": "Bearer " + CurrentToken,
+	}
+	body := map[string]interface{} {
+		"context_uri": "spotify:" + playType + ":" + playId,
+	}
+
+	response, err := makeRequest("PUT", BaseUrl + path, headers, body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if response.StatusCode != http.StatusNoContent {
+		return errors.New("featured playlists returned " + strconv.Itoa(response.StatusCode))
+	}
+	if response.StatusCode == http.StatusUnauthorized {
+		return errors.New("you need to re-login")
+	}
+
+	return nil
+}
+
+func getFeaturedPlaylists() (playlists []Playlist, err error) {
+	path := "/browse/featured-playlists?limit=50"
+	headers := map[string]string{
+		"Authorization": "Bearer " + CurrentToken,
+	}
+
+	response, err := makeRequest("GET", BaseUrl + path, headers, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if response.StatusCode != http.StatusOK {
+		return nil, errors.New("featured playlists returned " + strconv.Itoa(response.StatusCode))
+	}
+	if response.StatusCode == http.StatusUnauthorized {
+		return nil, errors.New("you need to re-login")
+	}
+
+	tempBody, _ := ioutil.ReadAll(response.Body)
+	var resBody FeaturedPlaylistsResponse
+	if jsonErr := json.Unmarshal(tempBody, &resBody); jsonErr != nil {
+		return nil, errors.New("cannot decode playlists")
+	}
+
+	return resBody.Playlists.Items, nil
+}
+
+/*
+ * SYSTEM FUNCTIONS
+ */
 
 func openBrowser(url string) {
 	var err error
@@ -135,7 +332,8 @@ func getToken(file *os.File) (token string, err error) {
 	if err != nil {
 		return "", err
 	}
-	return string(content), nil
+	CurrentToken = string(content)
+	return CurrentToken, nil
 }
 
 func findTempFileLocation() (f string, err error) {
@@ -164,7 +362,12 @@ func processCommand(args []string) {
 			if fileErr != nil {
 				log.Fatal(err)
 			}
-			println(v(file))
+			responseText := v(file)
+			if strings.HasSuffix(responseText, "\n") {
+				print(responseText)
+			} else {
+				println(responseText)
+			}
 			return
 		}
 	}
@@ -173,6 +376,34 @@ func processCommand(args []string) {
 		println("    " + k)
 	}
 	println()
+}
+
+func makeRequest(method string, url string, headers map[string]string, body map[string]interface{}, ) (response *http.Response, err error) {
+	var jsonParsed io.Reader
+
+	if body == nil {
+		jsonParsed = nil
+	} else {
+		jsonStr, err := json.Marshal(body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		jsonParsed = bytes.NewBuffer(jsonStr)
+	}
+
+	req, err := http.NewRequest(method, url, jsonParsed)
+	if err != nil {
+		log.Fatal("Cannot create request with url " + url)
+	}
+
+	if headers != nil {
+		for k, v := range headers {
+			req.Header.Add(k, v)
+		}
+	}
+
+	client := &http.Client{}
+	return client.Do(req)
 }
 
 func main() {
